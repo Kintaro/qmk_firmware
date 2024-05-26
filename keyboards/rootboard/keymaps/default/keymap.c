857b301396adeded02d5978768f74fb159a0dbcf
Simon Wollwage <mail.wollwage@gmail.com>
2024-05-27 03:42:33 +0900

    update

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_J,    KC_U,    KC_O,    KC_U,    KC_DOT,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_M,    KC_H,    KC_A,    KC_E,    KC_I,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_Z,    KC_L,    KC_COMM, KC_DOT,  KC_COMM,
-                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_SPC
+                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_Q,  KC_W,  KC_SPC
     )
 };
 
