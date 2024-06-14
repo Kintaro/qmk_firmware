2cd4ae5e79dd1394501413cb38ac20de91ae6a2a
Simon Wollwage <mail.wollwage@gmail.com>
2024-06-15 02:11:59 +0900

    update

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_X,    KC_F,    KC_D,    KC_P,    KC_H,                               KC_P,    KC_W,    KC_O,    KC_U,    KC_J,
 			     LCTL_T(KC_N),    LALT_T(KC_S),    LGUI_T(KC_T),    LSFT_T(KC_L),    KC_G,                               KC_Y,    LSFT_T(KC_H),    LGUI_T(KC_A),    LALT_T(KC_E),    LCTL_T(KC_I),
         KC_B,    KC_V,    KC_K,    KC_M,    KC_Q,                               KC_Z,    KC_C,    KC_QUOT, KC_SLASH,  KC_COMM,
-                             LT(2, KC_R), KC_BSPC, LT(1, KC_TAB),           LT(1, KC_Q),  KC_ENT,  KC_SPC
+                             LT(2, KC_R), KC_BSPC, LT(1, KC_TAB),           LT(1, KC_Q),  KC_ENT,  LT(2, KC_SPC)
 			     ),
     [1] = LAYOUT_split_3x5_3(
         KC_Q,    KC_1,    KC_2,    KC_3,    KC_T,                               KC_J,    KC_QUOTE,    KC_O,    KC_U,    KC_DOT,
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
+  //debug_enable=true;
+  //debug_matrix=true;
   //debug_keyboard=true;
   //debug_mouse=true;
 }
