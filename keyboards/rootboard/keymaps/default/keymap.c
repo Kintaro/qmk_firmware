4a73feaf9f2bbe8c7a9e7b2b212327b9de70e015
Simon Wollwage <mail.wollwage@gmail.com>
2024-08-05 04:11:18 +0900

    Update keymap.c

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -96,8 +96,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
       break;
     }
     }
-
-    history = keycode;
+    if (cont)
+      history = keycode;
     return cont;
 }
 
