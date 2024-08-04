7bdf8d1876ae0dc32ed4183115190c2f02024151
Simon Wollwage <mail.wollwage@gmail.com>
2024-08-05 01:55:12 +0900

    Update keymap.c

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -3,6 +3,15 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  FMAGIC = SAFE_RANGE,
+  VMAGIC,
+  BMAGIC,
+  HMAGIC,
+  EMAGIC,
+  YMAGIC,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
@@ -19,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [0] = LAYOUT_split_3x5_3(
-        KC_X,         KC_F,         KC_D,         KC_P,         KC_H,                    KC_P,          KC_W,         KC_O,         KC_U,         KC_J,
-	LCTL_T(KC_N), LALT_T(KC_S), LGUI_T(KC_T), LSFT_T(KC_L), KC_G,                    KC_Y,          LSFT_T(KC_H), LGUI_T(KC_A), LALT_T(KC_E), LCTL_T(KC_I),
-        KC_B,         KC_V,         KC_K,         KC_M,         KC_Q,                    KC_Z,          KC_C,         KC_QUOT,      KC_SLASH,     KC_COMM,
+        KC_X,         KC_F,         KC_D,         KC_P,         KC_J,                    QK_AREP,          KC_G,         KC_O,         KC_U,         KC_COMM,
+	LCTL_T(KC_N), LALT_T(KC_S), LGUI_T(KC_T), LSFT_T(KC_L), KC_W,                    KC_Y,          LSFT_T(KC_H), LGUI_T(KC_A), LALT_T(KC_E), LCTL_T(KC_I),
+        KC_B,         KC_V,         KC_K,         KC_M,         KC_Q,                    KC_Z,          KC_C,         KC_QUOT,      KC_SLASH,     KC_DOT,
                                     LT(2, KC_R),  KC_BSPC,      LT(1, KC_TAB),           LT(2, KC_DOT), KC_ENT,       LT(1, KC_SPC)
 			     ),
     [1] = LAYOUT_split_3x5_3(
@@ -38,6 +47,71 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    QK_BOOTLOADER, KC_BSPC,          KC_SPC,           LT(1, KC_Q),  KC_ENT,  KC_ESC
     )
 };
+
+uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_C:
+    }
+}
+enum custom_keycodes {
+  FMAGIC = SAFE_RANGE,
+  VMAGIC,
+  BMAGIC,
+  HMAGIC,
+  EMAGIC,
+  YMAGIC,
+};
+
+static void process_f_magic(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_C: KC_H; break;
+        case KC_P: KC_L; break;
+    }
+}
+
+static void process_v_magic(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_M: KC_P; break;
+        case KC_C: KC_Y; break;
+    }
+}
+
+static void process_b_magic(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_G: KC_H; break;
+    }
+}
+
+static void process_h_magic(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_U: KC_E; break;
+        case KC_L: KC_P; break;
+    }
+}
+
+static void process_p_magic(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_H: KC_Y; break;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (!process_achordion(keycode, record)) { return false; }
+  if (!process_custom_shift_keys(keycode, record)) { return false; }
+ 
+  if (record->event.pressed) {
+  switch (keycode) {
+        case FMAGIC: { process_f_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
+        case VMAGIC: { process_v_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
+      case BMAGIC: { process_b_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
+      case HMAGIC: { process_h_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
+      case PMAGIC: { process_p_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
+        }
+    }
+
+    return true;
+}
+
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
