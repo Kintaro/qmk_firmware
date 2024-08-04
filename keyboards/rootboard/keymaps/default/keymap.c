ca2650f46d97cab537ac1915d8a4b109c5782f81
Simon Wollwage <mail.wollwage@gmail.com>
2024-08-05 02:24:59 +0900

    Update keymap.c

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -29,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [0] = LAYOUT_split_3x5_3(
-        KC_X,         KC_F,         KC_D,         KC_P,         KC_J,                    QK_AREP,          KC_G,         KC_O,         KC_U,         KC_COMM,
-	LCTL_T(KC_N), LALT_T(KC_S), LGUI_T(KC_T), LSFT_T(KC_L), KC_W,                    KC_Y,          LSFT_T(KC_H), LGUI_T(KC_A), LALT_T(KC_E), LCTL_T(KC_I),
-        KC_B,         KC_V,         KC_K,         KC_M,         KC_Q,                    KC_Z,          KC_C,         KC_QUOT,      KC_SLASH,     KC_DOT,
+        KC_X,         FMAGIC,         KC_D,         PMAGIC,         KC_J,                    QK_AREP,          KC_G,         KC_O,         KC_U,         KC_COMM,
+	LCTL_T(KC_N), LALT_T(KC_S), LGUI_T(KC_T), LSFT_T(KC_L), KC_W,                    YMAGIC,          LSFT_T(HMAGIC), LGUI_T(KC_A), LALT_T(EMAGIC), LCTL_T(KC_I),
+        BMAGIC,         VMAGIC,         KC_K,         KC_M,         KC_Q,                    KC_Z,          KC_C,         KC_QUOT,      KC_SLASH,     KC_DOT,
                                     LT(2, KC_R),  KC_BSPC,      LT(1, KC_TAB),           LT(2, KC_DOT), KC_ENT,       LT(1, KC_SPC)
 			     ),
     [1] = LAYOUT_split_3x5_3(
@@ -63,6 +63,7 @@ static void process_f_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_C: SEND_STRING("h"); break;
         case KC_P: SEND_STRING("l"); break;
+        default: SEND_STRING("f"); break;    
     }
 }
 
@@ -70,12 +71,14 @@ static void process_v_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_M: SEND_STRING("p"); break;
         case KC_C: SEND_STRING("y"); break;
+        default: SEND_STRING("v"); break; 
     }
 }
 
 static void process_b_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_G: SEND_STRING("h"); break;
+        default: SEND_STRING("b"); break; 
     }
 }
 
@@ -83,24 +86,28 @@ static void process_h_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_U: SEND_STRING("e"); break;
         case KC_L: SEND_STRING("p"); break;
+        default: SEND_STRING("h"); break; 
     }
 }
 
 static void process_p_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_H: SEND_STRING("y"); break;
+        default: SEND_STRING("p"); break; 
     }
 }
 
 static void process_e_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_U: SEND_STRING("h"); break;
+        default: SEND_STRING("e"); break; 
     }
 }
 
 static void process_y_magic(uint16_t keycode, uint8_t mods) {
     switch (keycode) {
         case KC_C: SEND_STRING("v"); break;
+        default: SEND_STRING("y"); break; 
     }
 }
 
