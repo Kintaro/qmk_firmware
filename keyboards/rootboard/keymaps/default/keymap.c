be1b63964358eeccabb8b96ce929174ef78521ac
Simon Wollwage <mail.wollwage@gmail.com>
2024-08-07 03:10:04 +0900

    Update keymap.c

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -135,7 +135,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 	}
 
 
-	if (record->event.pressed) && !skip {
+	if (record->event.pressed && !skip) {
         predecessor_key = keycode;
 	}
 
