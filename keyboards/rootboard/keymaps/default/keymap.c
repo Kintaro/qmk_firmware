b8ed053710439b9a27f80905145706c8479ff709
Simon Wollwage <mail.wollwage@gmail.com>
2024-08-07 17:02:39 +0900

    Update keymap.c

--- keyboards/rootboard/keymaps/default/keymap.c
+++ keyboards/rootboard/keymaps/default/keymap.c
@@ -114,12 +114,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
       if (record->event.pressed) { // When MYMACRO is pressed.
           tap_code16(KC_C);
           set_oneshot_layer(1, ONESHOT_START);
+          clear_oneshot_layer_state(ONESHOT_PRESSED);
       }
       return false;
     case MM_P:
       if (record->event.pressed) { // When MYMACRO is pressed.
           tap_code16(KC_P);
           set_oneshot_layer(2, ONESHOT_START);
+          clear_oneshot_layer_state(ONESHOT_PRESSED);
       }
       return false;
   }
